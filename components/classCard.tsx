dd719e37dcf1409d77dce276944c2a636037685e
Fede-Martorell <federicomartorell.ltrc@gmail.com>
2025-05-06 19:25:41 -0300

    classCard y resourcesCard, cargue imagenes para las resourcesCard

---
+++ components/classCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import Image from "next/image";
+
+interface classCardProps {
+  subject: string; //nombre de clase o asignatura
+  students: number; //cantidad de estudiantes de la asignatura
+  tutors: number; //cantidad de tutores en la asignatura
+  classImage: string; //imagen de la asignatura
+}
+
+function classCard({ subject, students, tutors, classImage }: classCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <Image src={classImage} alt="Class Image"/>
+        <CardTitle>{subject}</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{students}</p>
+        <p>{tutors}</p>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default classCard;
