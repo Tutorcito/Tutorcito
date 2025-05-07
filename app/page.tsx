633c7134130c715b44043d6bcc76f573281b0f72
Fede-Martorell <federicomartorell.ltrc@gmail.com>
2025-05-06 21:48:35 -0300

    resourcesCard con tailwind, y carga de imagenes en page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
-import ClassCard from "@/components/classCard";
+
 import ResourcesCard from "@/components/resourcesCard";
+import ExcalidrawLogo from '../public/Excalidraw.png';
+import NotebookLMLogo from '../public/NotebookLM.png';
+
 
 export default function Home() {
   return (
-    <div className="flex  gap-4 min-w-screen">
-      <ResourcesCard icon="/Excalidraw.png" description1="Hola wachin" description2="Que tal wachin" description3="chau wachin" />
-      <ClassCard subject="comunicaciones" students={2000} tutors={6500} classImage="/Excalidraw.png" />
+    <div className="flex gap-4 min-w-screen">
+      <ResourcesCard 
+      icon={ExcalidrawLogo.src} 
+      description1="Mapas mentales" 
+      description2="Diagrama de flujo" 
+      description3="Anotaciones" />
+      <ResourcesCard icon={NotebookLMLogo.src} 
+      description1="Resumenes con IA" 
+      description2="Materias teoricas" 
+      description3="Chatbot Personalizado" />
+      <ResourcesCard icon="/Geogebra.png" 
+      description1="Graficos" 
+      description2="Analisis matematico" 
+      description3="Animaciones de graficos" />
+      <ResourcesCard icon="/Anki.png" 
+      description1="Flashcards" 
+      description2="Repaso de examen" 
+      description3="Memorizacion de conceptos" />
     </div>
   );
 }
