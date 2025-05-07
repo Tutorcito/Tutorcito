a562bb7c526ec1bd6faed481d22b6e4fbf3b6afe
Facundo-Perelló-04 <facuperello04@gmail.com>
2025-05-07 13:44:00 -0300

    implementacion de cartasCta

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import ResourcesCard from "@/components/resourcesCard";
 import ExcalidrawLogo from '../public/Excalidraw.png';
 import NotebookLMLogo from '../public/NotebookLM.png';
 import ResourceCarrusel from "@/components/ResourceCarrusel";
+import CardsCta from "@/components/cardsCta";
 
 
 export default function Home() {
   return (
-    <ResourceCarrusel/>
+    
+    <CardsCta/>
 
    
   );
