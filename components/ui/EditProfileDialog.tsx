6d9a85193238637583301a50dbceb581c7f67c45
maximoelst <maxelst12@gmail.com>
2025-05-14 11:07:41 -0300

    agregue el import en page de tutor y la creación de la carpeta EditProfileDialog.tsx para agregar el componente agregue <EditProfileDialog />despues de return para que salga el boton

--- components/ui/EditProfileDialog.tsx
+++ components/ui/EditProfileDialog.tsx
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export function EditProfileDialog() {
+export function EditProfileDialog() { 
   return (
     <Dialog>
       <DialogTrigger asChild>
